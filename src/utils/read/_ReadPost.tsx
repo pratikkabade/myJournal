82f7d2fe65cc9c34b19aeab0cf2d6bfd26608bd5
Pratik Kabade <pratikkabade@hotmail.com>
2022-11-28 01:04:00 +0530

    Removed Messed Files

--- src/utils/read/_ReadPost.tsx
+++
@@ -1,8 +0,0 @@
-export const _ReadPost = ({ id, logo, name, val }: any) => {
-    return (
-        <div key={id}>
-            <label key={id}><input type='checkbox' />
-                <i className={logo}></i> <kbd>{name}</kbd></label>
-        </div>
-    )
-}
