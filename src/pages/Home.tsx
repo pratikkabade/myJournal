761c414e18ffdeb4cab9ed273b841136a80bd2bc
Pratik Kabade <pratikkabade@hotmail.com>
2022-11-30 12:13:28 +0530

    Authentication from Sheets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../config/Firebase";
 import { SignIn } from "../security/SignIn"
@@ -5,18 +6,66 @@ import { Dashboard } from "./Dashboard";
 
 export const Home = () => {
     const [user] = useAuthState(auth);
+
+    // sheet
+    const [email, setEmail] = useState([])
+
+    // SHEET FUNCTIONS
+    const url = "https://script.google.com/macros/s/AKfycbzcUU_Qa6vthx_X-bBZcoALtOe5coqAc8bsOFFeFxCKH1oDGUGzQCVWL_NDKvo7W45iuw/exec";
+
+    const fetchData = async () => {
+        const response = await fetch(url);
+        const values = await response.json();
+
+        const emailValues = values.users;
+
+        // split val  by ,
+        const rVal = emailValues.map((item: any) => {
+            return {
+                item: item.split('|||')
+            }
+        })
+
+        // return item vise allotment
+        const rVal2 = rVal.map((item: any) => {
+            return {
+                email: item.item[0],
+            }
+        })
+
+        // DONT RETURN EMPTY VALUES
+        const finalVal = rVal2.filter((item: any) => {
+            return item.email !== ''
+        })
+
+        // CHECK FOR USER
+        const finalVal2 = finalVal.filter((item: any) => {
+            return item.email === user?.email
+        })
+
+        console.log(finalVal2);
+        setEmail(finalVal2)
+    }
+
+    // USE EFFECT
+    useEffect(() => {
+        fetchData()
+    })
+
     return (
         <>
-            {user?.email === 'thisispratikkabade@gmail.com' ?
-                <div>
-                    <Dashboard />
-                </div>
-                :
-                <div className="d-flex justify-content-center align-items-center flex-column noSelect" style={{ height: '80vh' }}>
-                    <h1><span className="text-danger">Partially Restricted</span> as for <span className="text-success">now</span></h1>
-                    <br /><br />
-                    <SignIn />
-                </div>
+            {
+                email.length !== 0
+                    ?
+                    <div>
+                        <Dashboard />
+                    </div>
+                    :
+                    <div className="d-flex justify-content-center align-items-center flex-column noSelect" style={{ height: '80vh' }}>
+                        <h1><span className="text-danger">Partially Restricted</span> as for <span className="text-success">now</span></h1>
+                        <br /><br />
+                        <SignIn />
+                    </div>
             }
         </>
     )
