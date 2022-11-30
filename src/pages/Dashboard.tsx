fa2ccf44581232ad94b6b527797b5bdd8c46c3e1
Pratik Kabade <pratikkabade@hotmail.com>
2022-11-30 10:26:02 +0530

    Created `Dashboard`

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+
+export const Dashboard = () => {
+    return (
+        <>
+            <div className="mainSection">
+                <div className="subSection">
+                    <h1>Dashboard</h1>
+                </div>
+            </div>
+            <div className="mainSection">
+                <div className="subSection d-flex justify-content-center">
+                    <Link to={"/myJournal/Journal"} className="btn-accent green blk">Journal 📃</Link>
+                </div>
+                <div className="subSection d-flex justify-content-center">
+                    <Link to={"/myJournal/Calendar"} className="btn-accent blue blk">Calendar 📆</Link>
+                </div>
+                <div className="subSection d-flex justify-content-center">
+                    <Link to={"/myJournal/Records"} className="btn-accent purple blk">Records 📂</Link>
+                </div>
+            </div>
+
+        </>
+    )
+}
